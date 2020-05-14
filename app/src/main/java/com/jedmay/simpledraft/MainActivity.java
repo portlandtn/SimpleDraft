2a98c90b0024465375bfa871b9927d4dd190ffec
Jedidiah May <jedmay83@gmail.com>
2020-05-14 10:43:22 -0500

    Add recyclerviews to main activity

--- app/src/main/java/com/jedmay/simpledraft/MainActivity.java
+++ app/src/main/java/com/jedmay/simpledraft/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jedmay.simpledraft;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.jedmay.simpledraft.adapters.OutputListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        populateRecyclerView(R.id.outputView1);
+        populateRecyclerView(R.id.outputView2);
+    }
+
+    private void populateRecyclerView(int viewId) {
+        RecyclerView outputView = findViewById(viewId);
+        final OutputListAdapter adapter = new OutputListAdapter(this, viewId);
+        outputView.setAdapter(adapter);
+        outputView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
