a87a4a96343be766ce43dd0e588ed13b2a38790f
jedidiahmay <jedmay83@gmail.com>
2020-05-16 12:41:13 -0500

    Start onClickListenerSetup. Time to merge branches.

--- app/src/main/java/com/jedmay/simpledraft/MainActivity.java
+++ app/src/main/java/com/jedmay/simpledraft/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.jedmay.simpledraft.db.SimpleDraftDbBadCompany;
@@ -21,10 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
     RadioButton angle1RadioButton, angle2RadioButton, angle3RadioButton, angle4RadioButton;
     Spinner output1Spinner, output2Spinner;
+    ListView outputListView1, outputListView2;
+    Button calculateWeightButton, setSlopeButton, enterAngleButton, deleteButton, clearButton,
+    footToDecimalButton, decimalToFootButton,
+    riseToSlopeButton, riseToBaseButton, baseToSlopeButton, baseToRiseButton, slopeToBaseButton, slopeToRiseButton;
+    Button divideButton, multiplyButton, minusButton, plusButton, enterButton, decimalButton;
+    Button oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, zeroButton;
+
+    Switch mathMethod;
 
     SimpleDraftDbBadCompany db;
     SampleDbData sampleDbData;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +49,35 @@ public class MainActivity extends AppCompatActivity {
         findAllViews();
 
         populateOutputSpinners();
-//        populateSpinner(output2Spinner);
+
+        setOnClickListeners();
 
     }
 
-    private void findAllViews() {
-        angle1RadioButton = findViewById(R.id.angle1RadioButton);
-        angle2RadioButton = findViewById(R.id.angle2RadioButton);
-        angle3RadioButton = findViewById(R.id.angle3RadioButton);
-        angle4RadioButton = findViewById(R.id.angle4RadioButton);
+    private void setOnClickListeners() {
 
-        output1Spinner = findViewById(R.id.output1NameSpinner);
-        output2Spinner = findViewById(R.id.output2NameSpinner);
+        Button[] numberButtons = new Button[]{oneButton,twoButton,threeButton,fourButton,fiveButton,sixButton,sevenButton,eightButton,nineButton,zeroButton};
+        setNumberButtonOnClickListener(numberButtons);
+        Button[] arithmeticButtons = new Button[]{minusButton,plusButton,divideButton,multiplyButton};
+        Button[] trigonometryButtons = new Button[]{riseToBaseButton,riseToSlopeButton,baseToRiseButton,baseToSlopeButton,slopeToRiseButton,slopeToBaseButton};
+        Button[] fragmentButtons = new Button[]{calculateWeightButton,setSlopeButton,enterAngleButton};
+        Button[] clearScreenButtons = new Button[]{deleteButton,clearButton};
+
     }
+
+    private void setNumberButtonOnClickListener(Button[] numberButtons) {
+        for(Button button : numberButtons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+
+    }
+
 
     private void populateOutputSpinners() {
         try {
@@ -71,9 +98,70 @@ public class MainActivity extends AppCompatActivity {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             output1Spinner.setAdapter(adapter);
             output2Spinner.setAdapter(adapter);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            Log.d("pop_index", ex.getLocalizedMessage());
         } catch (Exception ex) {
-            Log.d("pop_Spinner", ex.getLocalizedMessage());
+            Log.d("pop_general", ex.getLocalizedMessage());
         }
+    }
+
+    private void findAllViews() {
+        // Radio buttons
+        angle1RadioButton = findViewById(R.id.angle1RadioButton);
+        angle2RadioButton = findViewById(R.id.angle2RadioButton);
+        angle3RadioButton = findViewById(R.id.angle3RadioButton);
+        angle4RadioButton = findViewById(R.id.angle4RadioButton);
+
+        // Math method switch
+        mathMethod = findViewById(R.id.mathMethodSwitch);
+
+        // Spinners
+        output1Spinner = findViewById(R.id.output1NameSpinner);
+        output2Spinner = findViewById(R.id.output2NameSpinner);
+
+        //List Views
+        outputListView1 = findViewById(R.id.output1ListView);
+        outputListView2 = findViewById(R.id.output2ListView);
+
+        //Buttons
+        calculateWeightButton = findViewById(R.id.calculateWeightButton);
+        setSlopeButton = findViewById(R.id.setRoofSlopeButton);
+        enterAngleButton = findViewById(R.id.setCustomAngleButton);
+        deleteButton = findViewById(R.id.deleteButton);
+        clearButton = findViewById(R.id.clearButton);
+
+        // Conversion Buttons
+        footToDecimalButton = findViewById(R.id.footToDecimal);
+        decimalToFootButton = findViewById(R.id.decimalToFoot);
+
+        // Trig Buttons
+        riseToSlopeButton = findViewById(R.id.riseToSlopeButton);
+        riseToBaseButton = findViewById(R.id.riseToBaseButton);
+        baseToSlopeButton = findViewById(R.id.baseToSlopeButton);
+        baseToRiseButton = findViewById(R.id.baseToRiseButton);
+        slopeToBaseButton = findViewById(R.id.slopeToRiseButton);
+        slopeToRiseButton = findViewById(R.id.slopeToBaseButton);
+
+        // Math Buttons
+        divideButton = findViewById(R.id.divideButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        minusButton = findViewById(R.id.minusButton);
+        plusButton = findViewById(R.id.plusButton);
+        decimalButton = findViewById(R.id.decimalButton);
+        enterButton = findViewById(R.id.enterButton);
+
+        // Number Buttons
+        oneButton = findViewById(R.id.oneButton);
+        twoButton = findViewById(R.id.twoButton);
+        threeButton = findViewById(R.id.threeButton);
+        fourButton = findViewById(R.id.fourButton);
+        fiveButton = findViewById(R.id.fiveButton);
+        sixButton = findViewById(R.id.sixButton);
+        sevenButton = findViewById(R.id.sevenButton);
+        eightButton = findViewById(R.id.eightButton);
+        nineButton = findViewById(R.id.nineButton);
+        zeroButton = findViewById(R.id.zeroButton);
+
     }
 
 }
