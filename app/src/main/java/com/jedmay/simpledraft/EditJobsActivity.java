75e032182a65bc1e3e2c695f653c8f747a156693
jedidiahmay <jedmay83@gmail.com>
2020-05-29 18:39:26 -0500

    Edit Jobs Layout Complete

---
+++ app/src/main/java/com/jedmay/simpledraft/EditJobsActivity.java
@@ -0,0 +1,14 @@
+package com.jedmay.simpledraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditJobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_jobs);
+    }
+}
