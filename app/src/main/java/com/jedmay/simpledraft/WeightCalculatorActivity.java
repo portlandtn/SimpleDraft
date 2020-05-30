100a74f993b521a1f968bd3463b9860db29fd15d
jedidiahmay <jedmay83@gmail.com>
2020-05-30 17:10:49 -0500

    Weight calc complete

--- app/src/main/java/com/jedmay/simpledraft/WeightCalculatorActivity.java
+++ app/src/main/java/com/jedmay/simpledraft/WeightCalculatorActivity.java
@@ -8,36 +8,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jedmay.simpledraft.helper.Arithmetic;
 import com.jedmay.simpledraft.helper.Constants;
 import com.jedmay.simpledraft.helper.Converters;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 public class WeightCalculatorActivity extends AppCompatActivity {
 
     Switch lengthSwitch, widthSwitch, thicknessSwitch;
     EditText lengthEditText, widthEditText, thicknessEditText;
-    TextView weightTextView;
-    Button cancelButton, pushButton;
+    TextView weightTextView, weightTotalTextView;
+    ListView weightListView;
+    Button cancelButton, pushButton, clearAllWeightsButton;
+    List<Double> weightListValues;
     int activeWindow;
-    double weight;
+    double currentWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_calculator);
 
+        weightListValues = new ArrayList<>();
+
         Intent intent = getIntent();
         activeWindow = intent.getIntExtra(Constants.activeWindow, 1);
-        weight = 0;
+        currentWeight = 0;
         findAllViews();
+        updateWeightListView();
         setTextWatchers();
         setSwitchListeners();
         setButtonListeners();
+        setWeightListViewListeners();
     }
 
     private void setTextWatchers() {
@@ -54,7 +69,7 @@ public class WeightCalculatorActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                updateWeightTextView();
+                updateCurrentWeight();
             }
         });
         widthEditText.addTextChangedListener(new TextWatcher() {
@@ -70,7 +85,7 @@ public class WeightCalculatorActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                updateWeightTextView();
+                updateCurrentWeight();
             }
         });
         thicknessEditText.addTextChangedListener(new TextWatcher() {
@@ -86,13 +101,13 @@ public class WeightCalculatorActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                updateWeightTextView();
+                updateCurrentWeight();
             }
         });
     }
 
     @SuppressLint("DefaultLocale")
-    private void updateWeightTextView() {
+    private void updateCurrentWeight() {
         double length, width, thickness;
         try {
             length = Double.parseDouble(lengthEditText.getText().toString());
@@ -118,32 +133,41 @@ public class WeightCalculatorActivity extends AppCompatActivity {
             response = Arithmetic.calculateWeightUsingInchDimensions(length * 12, width * 12, thickness * 12);
         }
         weightTextView.setText(String.format("%.4f",response));
+        currentWeight = response;
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void updateWeightTotalTextView() {
+        double totalWeight = 0;
+        for(Double value : weightListValues){
+            totalWeight += value;
+        }
+        weightTotalTextView.setText(String.format("%.4f",totalWeight));
     }
 
     private void setSwitchListeners() {
         lengthSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             lengthSwitch.setText(lengthSwitch.isChecked() ? "Detailing" : "Standard");
-            updateWeightTextView();
+            updateCurrentWeight();
         });
 
         widthSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             widthSwitch.setText(widthSwitch.isChecked() ? "Detailing" : "Standard");
-            updateWeightTextView();
+            updateCurrentWeight();
         });
 
         thicknessSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             thicknessSwitch.setText(thicknessSwitch.isChecked() ? "Detailing" : "Standard");
-            updateWeightTextView();
+            updateCurrentWeight();
         });
     }
 
     private void setButtonListeners() {
         pushButton.setOnClickListener(v-> {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            intent.putExtra(Constants.weight,weight);
-            intent.putExtra(Constants.activeWindow, activeWindow);
-            startActivity(intent);
-            finish();
+            weightListValues.add(currentWeight);
+            updateWeightTotalTextView();
+            updateWeightListView();
+            resetValuesForNewEntry();
         });
         cancelButton.setOnClickListener(v-> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -158,7 +182,73 @@ public class WeightCalculatorActivity extends AppCompatActivity {
             builder.setNegativeButton("No",((dialog, which) -> {
                 dialog.cancel();
             }));
+            builder.show();
         });
+        clearAllWeightsButton.setOnClickListener(v-> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Clear all weights?");
+            builder.setMessage("Are you sure you want to clear the weights and start over?");
+            builder.setPositiveButton("Yes",((dialog, which) -> {
+                clearAllValues();
+            }));
+            builder.setNegativeButton("No",((dialog, which) -> {
+                dialog.cancel();
+            }));
+            builder.show();
+        });
+    }
+
+    private void setWeightListViewListeners() {
+        weightListView.setOnItemClickListener((parent, view, position, id) -> {
+
+            Double value = (Double) weightListView.getAdapter().getItem(position);
+            AlertDialog.Builder builder = new AlertDialog.Builder(WeightCalculatorActivity.this);
+            builder.setTitle("Delete This Weight?");
+            builder.setMessage("Are you sure you want to delete " + value + "?");
+            builder.setPositiveButton("Yes",((dialog, which) -> {
+                for(int i = 0; i < weightListValues.size(); i++) {
+                    if (weightListValues.get(i).equals(value)) {
+                        weightListValues.remove(i);
+                        updateWeightListView();
+                        updateWeightTotalTextView();
+                        return;
+                    }
+                }
+            }));
+            builder.setNegativeButton("No",((dialog, which) -> {
+                dialog.cancel();
+            }));
+            builder.show();
+        });
+    }
+
+    private void resetValuesForNewEntry() {
+        lengthEditText.setText("");
+        widthEditText.setText("");
+        thicknessEditText.setText("");
+        weightTextView.setText(R.string._0_00);
+    }
+
+    private void clearAllValues() {
+        lengthEditText.setText("");
+        widthEditText.setText("");
+        thicknessEditText.setText("");
+        weightTextView.setText(R.string._0_00);
+        weightTotalTextView.setText(R.string._0_00);
+        weightListValues.clear();
+        updateWeightListView();
+        currentWeight = 0;
+    }
+
+    private void updateWeightListView() {
+        Double[] weightArray = new Double[weightListValues.size()];
+        for (int i = 0; i < weightListValues.size(); i++){
+            weightArray[i] = weightListValues.get(i);
+        }
+
+        ArrayAdapter<Double> adapter = new ArrayAdapter<>(this,R.layout.jobs_view_layout,R.id.jobsViewItem,weightArray);
+        weightListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     private void findAllViews() {
@@ -169,8 +259,11 @@ public class WeightCalculatorActivity extends AppCompatActivity {
         widthEditText = findViewById(R.id.widthEditText);
         thicknessEditText = findViewById(R.id.thicknessEditText);
         weightTextView = findViewById(R.id.weightTextView);
+        weightTotalTextView = findViewById(R.id.weightTotalTextView);
+        weightListView = findViewById(R.id.weightListView);
         cancelButton = findViewById(R.id.cancelWeightButton);
-        pushButton = findViewById(R.id.pushWeightButton);
+        pushButton = findViewById(R.id.pushToWeightListViewButton);
+        clearAllWeightsButton = findViewById(R.id.clearAllWeightsButton);
     }
 
 
